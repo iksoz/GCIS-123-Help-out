6f0a98d16263e564b8fbdd57a4bc619d01f39e6d
Zifeng Li <zifengli114@gmail.com>
2025-09-10 15:48:43 -0400

    test files

---
+++ classAct/hi.py
@@ -0,0 +1,8 @@
+def add(x,y):
+    return x + y
+
+def main():
+    print(add(2,1))
+
+if __name__ == "__main__":
+    main()
---
+++ classAct/hi_test.py
@@ -0,0 +1,8 @@
+import hi
+
+def test_add():
+    x = 1
+    y = 2
+    expected = 3
+    actual = hi.add(1,2)
+    assert actual == expected
